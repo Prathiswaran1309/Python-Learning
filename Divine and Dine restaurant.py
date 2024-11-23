fc1fe677cd677647a1eea7f1609e4cdf9a75096c
Prathiswaran D <miracleofgenius@gmail.com>
2024-11-23 17:24:37 +0530

    Delete Divine and Dine restaurant.py

--- Divine and Dine restaurant.py
+++
@@ -1,9 +0,0 @@
-print ("Welcome to the Dine & Divide!!")
-bill = int(input("What was the total bill in Rs:"))
-tip_per =  int(input("How much tip would you like to give? 10,12 or 15?"))
-tip = (tip_per*bill)/100
-Person = int(input("How many people to share the bill?"))
-Each_person = round((bill+tip)/Person,2)
-print(f"Each person should pay: {Each_person}")
-
-
