b91d9ed86ec66ecabd4e7a9d1c1cc1087d9c3884
Prathiswaran D <miracleofgenius@gmail.com>
2024-11-23 17:31:14 +0530

    Delete Finding the year of experience.py

--- Finding the year of experience.py
+++
@@ -1,6 +0,0 @@
-username = input("Enter the username: ")
-email_address = input("Enter the mail id: ")
-Year_of_Joining = int(input("Enter the year of joining: "))
-Year_of_Experience = 2024-Year_of_Joining
-print(f'Hello \"{username}\", We are reaching you at \'{email_address}\' and you have \'{Year_of_Experience}\' year experience')
-
