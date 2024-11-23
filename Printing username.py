9efe2071761b712f70b0b90d7c62e2258aa752dc
Prathiswaran D <miracleofgenius@gmail.com>
2024-11-23 17:25:05 +0530

    Delete Printing username.py

--- Printing username.py
+++
@@ -1,6 +0,0 @@
-#Getting the following input "Username" "Email ID" & "Year of Joining" and Display it.
-username = input ("Enter the username:")
-email_id = input ("Enter the mail_id:")
-Joining_year = int(input("Enter the joining date:"))
-c = 2024-Joining_year
-print (f"Hello \"{username}\" ,We are reaching you at \'{email_id}\' and you have \'{c}\' experience")
